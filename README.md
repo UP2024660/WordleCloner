abb243f510a8d2030912754339b5e2a0bb446a33
UP2024660 <77286481+UP2024660@users.noreply.github.com>
2022-07-22 03:28:00 +0100

    WordleClone

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# WordleCloner
-Better Wordle
+
+# Key Features
+
+* Keyboard Input while also offering the On Screen Keyboard functionality.
+
+* Keeps the Same Word even after refreshing the page.
+
+* Functionality of using either a Local Based List version of words or the SQL table version.
+
+* Statistics  - Played | Win % | Current Streak | Max Streak | total Wins | Total Losses |
+
+---
+
+# Design Implementations and Rationale
+
+* I used the same fonts and same colours as the original Wordle , for a sense of familiarity while playing my version.
+
+* I've gone for a pop up screen at the start that isn't too big nor small,so that users from any device can be comfortable seieng it, and therefor closing it.
+
+* I've included my To-Do-List and my Done-list , to show my progress throughout this project, and to areas in which I know i can, and if given time in the future can improve in newer updates.
+
+---
+
+## To-Do List
+
+* Extra colour for use of two letters?
+
+* Ability to Add Words to SQL table / List.
+
+* See User Guess History.
+
+* Keep Status of the Game, so User's can't cheat by refreshing the page when close to losing.
+
+## Done
+
+* Table shows up
+
+* OS Keyboard input works
+
+* Keyboard input for Words
+
+* Grey out board
+
+* resize
+
+* Not possible to delete already entered words
+
+* Hide the word for the day so you can't inspect element it
+
+---
--- server.js
+++ server.js
@@ -1,56 +1,75 @@
 // use the Express.js framework https://expressjs.com/
 import express from 'express';
-
-
-import uuid from 'uuid-random';
-import *  as sqlite from 'sqlite3';
-import * as wordBase from './sqlData.js';
-
-
-// const sqlite = require('sqlite');
-// import uuid from 'uuid';
-
-// import * as wordBase from './wordle.js';
+import { v4 as uuidv4 } from 'uuid';
+import schedule from 'node-schedule';
+import * as dbData from './sqlDataManager';
 
 const app = express();
+let wOtD, sID;
+dbData.chooseWord().then((word) => {
+  wordOtD = word;
+  sID = uuidv4();
+  app.listen(8080);
+});
 
-app.use(express.static('public' , { extensions: ['html']}));
+app.use(express.static('public'));
 
-// async function getWords(req,res){
-//     res.json(await wordBase.listWords());
-// }
+app.post('/checkword', express.json(), async (req, res) => {
+    let todWord = [];
+    if (!(await dbData.checkWord(req.body.guess))){ res.setHeader('Content-Type', 'application/json'); res.send(JSON.stringify(todWord));return;}
+    
+    todWord = wordChecker(req.body.guess, wordOtD.toUpperCase());
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(todWord));
+  });
+  // Chooses Todays Word, Randomly, out of the SQL table * or the local list version.
+  app.get('/chooseTodWord', (req, res) => { res.setHeader('Content-Type', 'application/json'); res.send(JSON.stringify(wordOtD)); console.log(wordOtD);
+  });
 
-// async function getWord(req,res){
-//     const result = await wordBase.findWord(req.params.word);
-//     if(!result){
-//         res.status(404).send('No match for this Word');
-//         return;
-//     }
-//     res.json(result);
-// }
+  //Keeps the Same word, even if you refresh the page
+  app.get('/sessionStatus', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(wordlSessionId));
+  });
 
-function responder(req, res) {
-    if (req.url === '/dyn1'){
-        const message = "pretty lil thing";
-        const date = new Date().toString();
+  // Changes the Word every 24 hours (or every restart of npm)
+  schedule.scheduleJob('0 0 0 * * *', async function () {
+    wordOtD = await dbData.chooseWord();
+    sIDD = uuidv4();
+  });
+  
+  // Regular functions that check the word, and check if letter is in word.
 
-        res.write(message + '\n' + date);}
-    else{
-        res.statusCode = 404;
-        res.write('Not Found!');
+  function wordChecker(userGuess, correctWord) {
+    const letValArr = ['0', '0', '0', '0', '0'];
+    userGuess = userGuess.toUpperCase();
+    const guessArr = userGuess.split('');
+    const wordOtDArr = correctWord.split('');
+    checkCharInWordPos(guessArr, wordOtDArr, letValArr);
+    checkCharInWordExists(guessArr, wordOtDArr, letValArr);
+    return letValArr;
+  }
+  
+  // Check if character is in correct position
+  function checkCharInWordPos(guessWordArr, wordOtDArr, letValArr) {
+    for (let i = 0; i < wordOtDArr.length; i++) {
+      if (wordOtDArr[i] === guessWordArr[i]) {
+        letValArr[i] = '2';
+        wordOtDArr[i] = '';
+        guessWordArr[i] = '';
+      }
     }
-    res.end();
-}
-
-
-function asyncWrap(f){
-    return (req,res, next) => {
-        Promise.resolve(f(req,res, next))
-            .catch((e) => next(e || new Error()));
-    };
-}
-
-app.listen(8080);
-
-
-
+  }
+  
+  // Check if character is in word
+  function checkCharInWordExists(guessWordArr, wordOtArr, letValArr) {
+    for (let i = 0; i < wordOtDArr.length; i++) {
+      if (guessWordArr[i] !== '' && wordOtDArr.includes(guessWordArr[i])) {
+        letValArr[i] = '1';
+        const char = guessWordArr[i];
+        const index = wordOtDArr.indexOf(char);
+        wordOtDArr[index] = '';
+        guessWordArr[i] = '';
+      }
+    }
+  }
